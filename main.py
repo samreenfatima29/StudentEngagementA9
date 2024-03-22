8fc8e0ff5a3c3dfefdbc5860ef7e913836e932f5
samreen <samreenfatima02912@gmail.com>
2024-03-22 10:50:14 +0530

    ml model

---
+++ main.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
+import time
+import csv
+
+def overlay_results(frame, label):
+    cv2.rectangle(frame, (10,10), (300,50), (0, 0, 0), -1)
+    cv2.putText(frame, label, (15,35), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), cv2.LINE_AA)
+    return frame
+
+class_labels = ['Not Engaged: Looking Away', 'Not Engaged: Bored', 'Engaged: Confused', 'Not Engaged: Drowsy', 'Engaged: engaged', 'Engaged: Frustrated']
+model = load_model('facial_recognizer.h5')
+
+cap = cv2.VideoCapture(0 )
+last_prediction_time = time.time() 
+
+with open('output.csv', 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(['Time', 'Prediction'])
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    current_time = time.time()  
+
+    if current_time - last_prediction_time >= 1:
+
+        # Frame preprocessing
+        frame_resized = cv2.resize(frame, (300, 300))
+        frame_normalized = frame_resized.astype(np.float32) / 255.0
+
+        predict_proba = model.predict(np.expand_dims(frame_normalized, axis = 0))
+        predict_class = np.argmax(predict_proba)
+        predict_label = class_labels[predict_class]
+
+        with open('output.csv', 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow([time.strftime('%Y-%m-%d %H:%M:%S'), predict_label])
+
+        last_prediction_time = current_time
+
+        frame_overlay = overlay_results(frame, predict_label)
+        cv2.imshow('Facial Expression Recognition', frame_overlay)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
